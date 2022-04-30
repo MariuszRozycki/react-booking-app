41b83459befa11ba312479ab066e22223012e619
MariuszRozycki <mariansc83@icloud.com>
2022-04-30 14:40:48 +0200

    feat: passing props

--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import PropTypes from 'prop-types';
+import PropTypes, { element } from 'prop-types';
 import './App.css';
 
 
@@ -10,7 +10,8 @@ const listBackground = {
 
 function Offer({
   name,
-  description,
+  street,
+  city,
   country,
   cover,
   handleClick
@@ -28,7 +29,7 @@ function Offer({
         >
           {name}
         </h2>
-        <p>{description}</p>
+        <p>{street}, {city}</p>
         <p>{country}</p>
       </div>
       <div className='offer__column'></div>
@@ -41,55 +42,71 @@ Offer.propTypes = {
 }
 
 Offer.defaultProps = {
-  description: "Brakuje opisu"
+  description: "Brakuje nazwy ulicy"
 }
 
+const offers = [{
+  "id": "1",
+  "name": "Six Suites, Old Town",
+  "street": "Kołodziejska 2",
+  "postal_code": "80-836",
+  "city": "Gdańsk",
+  "country_code": "PL",
+  "country_name": "Polska",
+  "cover": "https://cf.bstatic.com/xdata/images/hotel/square90/79172064.webp?k=9af8e5c75879a392d627595f8fad96d205e23ed264ec3744476b57805fa28731&o="
+}, {
+  "id": "2",
+  "name": "Hotel Arche Geologiczna",
+  "street": "ul. Geologiczna 4",
+  "postal_code": "02-246",
+  "city": "Warszawa",
+  "country_code": "PL",
+  "country_name": "Polska",
+  "cover": "https://cf.bstatic.com/xdata/images/hotel/square90/192541890.webp?k=179376106102b1cde30c1b2fd7f220f1047d3015427af40d04e0a7bb1ecd16eb&o="
+}, {
+  "id": "3",
+  "name": "Galeria Rondo Wiatraczna Apartments",
+  "street": "Grochowska 207",
+  "postal_code": "04-077",
+  "city": "Warszawa",
+  "country_code": "PL",
+  "country_name": "Polska",
+  "cover": "https://cf.bstatic.com/xdata/images/hotel/square90/164843364.webp?k=c9b83ddbec06f9602cd3acd75506a03bc5bf250384193109fe665682f780b3ce&o="
+}, {
+  "id": "4",
+  "name": "Hotel Miodowy Młyn",
+  "street": "Legionów 3",
+  "postal_code": "27-500",
+  "city": "Opatów",
+  "country_code": "PL",
+  "country_name": "Polska",
+  "cover": "https://cf.bstatic.com/xdata/images/hotel/square90/30873029.webp?k=dea5daa25f917081221d64f1e3cb8c0d1094c40dfc00b612b508973f597aba4e&o="
+},
+];
 
 function Home() {
 
   const [selectedOffer, setSelectedOffer] = useState("Cozy flat");
-  // const [data, setData] = useState([]);
 
   useEffect(() => {
     setSelectedOffer("!!!");
   }, []);
-
-  // useEffect(() => {
-  //   fetch("https://jsonplaceholder.typicode.com/posts")
-  //     .then(response => response.json())
-  //     .then(data => setData(data))
-  // }, []);
 
   return (
     <div className="App">
       <div>
         <h1>Wybrano: {selectedOffer}</h1>
       </div>
-      <Offer
-        name={"Hotel Goralski"}
-        description={"Spokojne miejsce w gorach"}
-        country={"Polska"}
-        cover="https://picsum.photos/150/150"
+
+      {offers.map(element => <Offer
+        key={`offer-${element.id}`}
+        name={element.name}
+        street={element.street}
+        city={element.city}
+        country={element.country_name}
+        cover={element.cover}
         handleClick={setSelectedOffer}
-      />
-      <Offer
-        name={"Hotel pod Jaworem"}
-        country={"Polska"}
-        cover="https://picsum.photos/150/150"
-        handleClick={setSelectedOffer}
-      />
-      <Offer
-        name={"Hotel na Skarpie"}
-        country={"Polska"}
-        cover="https://picsum.photos/150/150"
-        handleClick={setSelectedOffer}
-      />
-      <Offer
-        name={"Hotel Gdynski"}
-        country={"Polska"}
-        cover="https://picsum.photos/150/150"
-        handleClick={setSelectedOffer}
-      />
+      />)}
     </div>
   );
 }
